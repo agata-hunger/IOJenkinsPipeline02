ffd67a58aea2c044fe2c8d39eafb99bbd3fc3df7
agata-hunger <72617977+agata-hunger@users.noreply.github.com>
2023-04-26 12:35:57 +0200

    update src

--- src/main/java/JenkinsPipeline/JenkinsPipelineApplication.java
+++ src/main/java/JenkinsPipeline/JenkinsPipelineApplication.java
@@ -3,16 +3,17 @@ package JenkinsPipeline;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
+@RestController
 public class JenkinsPipelineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsPipelineApplication.class, args);
 	}
 
-	@GetMapping
+	@GetMapping("/")
 	public String get() {
 		return "Hello Jenkins!";
 	}
